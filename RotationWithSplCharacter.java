fcd33ab928ed08988d1432751a902ae5e7bb4d02
Barath2605 <55592058+Barath2605@users.noreply.github.com>
2020-03-27 20:58:39 +0530

    Update RotationWithSplCharacter.java

--- RotationWithSplCharacter.java
+++ RotationWithSplCharacter.java
@@ -1,19 +1,30 @@
 import java.util.*;
+import java.lang.*;
 class RotationWithSplCharacter{
     public static void main(String[] args) {
         Scanner sc=new Scanner (System.in);
         String a=sc.next();
-        //char[] b=a.toCharArray();
         int n=sc.nextInt();
         if(a.length()<n)
            n=n%a.length();
         String b="";
         for(int i=0;i<n;i++) {
             String[] c=a.split("",2);
-            if(Character.isLetter(c[0].charAt(0)))
+            if(Character.isAlphabetic(c[0].charAt(0))){
                 b=c[1].concat(c[0]);
-            else
-                b=c[0].concat(c[1]);
+		}
+            else{
+		String d=c[0].concat(c[1]);
+		for(int j=0;j<d.length();j++) {
+		if(Character.isAlphabetic(d.charAt(j))){
+//System.out.print("c------"+d.charAt(j));
+                	b=d.substring(0,j)+d.substring(j+1,d.length())+d.charAt(j);
+			break;
+		}
+		}
+				
+
+		}
             a=b;
         }
         System.out.println(a); 
